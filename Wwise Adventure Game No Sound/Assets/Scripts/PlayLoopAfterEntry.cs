7f9c7611f73727ccb189f1c7be1a28e58e4f0b1b
albertmas <almaco_io@hotmail.es>
2019-06-08 22:54:58 +0200

    Title Screen music

---
+++ Wwise Adventure Game No Sound/Assets/Scripts/PlayLoopAfterEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayLoopAfterEntry : MonoBehaviour {
+    public float loop_start;
+    public float loop_end;
+    AudioSource audiosource;
+
+	void Start () {
+        audiosource = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+        if (audiosource.time >= loop_end)
+            audiosource.time = loop_start;
+	}
+}
